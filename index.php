e11ece13b86b5553a4a61711771e0d8a726a86d3
jay150503maulana <fajarpro27@gmail.com>
2019-08-02 15:04:12 +0700

    saya melakukan perubhan yang banyak

--- index.php
+++ index.php
@@ -9,22 +9,32 @@
 </head>
 <body>
 <?php
-$a = 5;
-$b = 5;
+$a = 28;
+$b = 2;
 $c = $a+$b;
+if ($c <= 5){
+    echo '<h1 style = "color : red">'.$c.'</h1>';
+}elseif ($c <= 10){
+    echo '<h1 style = "color : blue">'.$c.'</h1>';
+}elseif ($c <= 15){
+    echo '<h1 style = "color : yellow">'.$c.'</h1>';
+}elseif ($c <= 20){
+    echo '<h1 style = "color : green">'.$c.'</h1>';
+}elseif ($c <= 25){
+    echo '<h1 style = "color : black">'.$c.'</h1>';
+}elseif ($c <= 30){
+    echo '<h1 style = "color : #6A5ACD">'.$c.'</h1>';
+}elseif ($c <= 35){
+    echo '<h1 style = "color : skyblue">'.$c.'</h1>';
+}elseif ($c <= 40){
+    echo '<h1 style = "color : pink">'.$c.'</h1>';
+}elseif ($c <= 45){
+    echo '<h1 style = "color : #b22222">'.$c.'</h1>';
+}elseif ($c <= 50){
+    echo '<h1 style = "color : white">'.$c.'</h1>';
+}
 ?>
-<h1 style ="color : red">
-<------- ISI VARIABLE -----><br>
-<?php
-echo "$c";
-?>
-</h1>
-<h1 style="color:red" >
-<------- ISI VARIABLE -----><br>
-<?php
-echo "$b";
-?>
-</h1>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
